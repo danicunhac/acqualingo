2d4f2a6d2b5c0593bd4da918c7033977f6a74a36
Daniel Cunha <danielcc.jp@gmail.com>
2023-10-08 10:55:26 -0300

    wip: home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,192 @@
-import Image from 'next/image'
+import { Inter } from 'next/font/google';
+import Image from 'next/image';
+
+const inter = Inter({ subsets: ['latin'] });
+
+const metadata = [
+  {
+    id: 1,
+    title: 'Precipitation',
+    text: 'À medida que as nuvens se tornam mais densas e pesadas, as gotas de água se unem e caem de volta à Terra na forma de chuva, neve ou granizo. Esse é o estágio da precipitação, que repõe a água nos reservatórios terrestres.. ',
+    imagePosition: 'right',
+  },
+  {
+    id: 2,
+    title: 'Evaporation',
+    text: 'Tudo começa com a energia do sol aquecendo a superfície da Terra. Essa energia é absorvida pela água em oceanos, lagos e rios, levando à evaporação das moléculas de água, que se transformam em vapor de água na atmosfera.',
+    imagePosition: 'left',
+  },
+  {
+    id: 3,
+    title: 'Condensation',
+    text: 'O vapor de água sobe na atmosfera, onde a temperatura é mais baixa. Isso leva à condensação, na qual o vapor se agrupa para formar pequenas gotas de água, criando nuvens. Esse processo é crucial para a formação das nuvens.',
+    imagePosition: 'right',
+  },
+  {
+    id: 4,
+    title: 'Infiltration',
+    text: 'A água da precipitação se infiltra no solo, onde é absorvida pelas plantas ou segue para os lençóis freáticos. Parte dela também flui para rios e riachos, alimentando ecossistemas aquáticos.',
+    imagePosition: 'left',
+  },
+  {
+    id: 5,
+    title: (
+      <>
+        (Evapo) <br /> Transpiration
+      </>
+    ),
+    text: 'As plantas absorvem a água do solo e a liberam na atmosfera através da transpiração. A evapotranspiração refere-se à soma da evaporação da superfície do solo e da transpiração das plantas. Esse processo é vital para a circulação contínua da água na biosfera.',
+    imagePosition: 'right',
+  },
+  {
+    id: 6,
+    title: 'Runoff to streams',
+    text: 'A água que não é absorvida pelo solo ou pelas plantas flui para rios e riachos, formando sistemas fluviais que fornecem água doce para diversas finalidades, desde o abastecimento público até a agricultura.',
+    imagePosition: 'left',
+  },
+  {
+    id: 7,
+    title: 'Runoff to ocean',
+    text: 'Finalmente, a água flui de volta para os oceanos, onde o ciclo recomeça com a evaporação.',
+    imagePosition: 'right',
+  },
+];
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+    <main className="flex min-h-screen flex-col items-center justify-between px-[120px] py-[60px] bg-[url('/ocean-background.png')] bg-center bg-no-repeat bg-cover w-full">
+      <div className="flex flex-col z-10 w-full items-center justify-between text-sm bg-white/10 backdrop-blur-sm rounded-lg pt-5 px-20 pb-20 gap-8 w-full">
+        <div className="gap-10 flex items-center justify-between fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:bg-none font-bold">
           <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+            className="text-2xl uppercase pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
+            Acqualingo
           </a>
+          <div className="flex gap-32 uppercase font-normal">
+            <a href="">Home</a>
+            <a href="#practice">Practice</a>
+            <a href="">Contact</a>
+            <a href="">About</a>
+          </div>
+        </div>
+
+        <div className="flex">
+          <div className="w-full flex flex-col gap-y-8 max-w-[900px]">
+            <h1 className="font-bold text-6xl max-w-[900px] leading-tight uppercase">
+              Everything <br /> starts with Water
+            </h1>
+            <p className={`${inter.className} text-lg font-extralight`}>
+              Embark on this adventure of discovery about water and the impacts
+              that our ecosystem has been suffering.
+            </p>
+            <button
+              type="button"
+              className="uppercase transition-colors text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 shadow-lg shadow-cyan-500/50 dark:shadow-lg dark:shadow-cyan-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+            >
+              Start
+            </button>
+          </div>
+
+          <Image
+            className="max-w-[50%]"
+            src="/cute-whale.svg"
+            alt="Cute whale"
+            width={600}
+            height={500}
+          />
+        </div>
+
+        <div className="flex flex-col mt-32 w-full gap-32">
+          {metadata.map((item) => (
+            <div key={item.id} className="flex justify-between">
+              {item.imagePosition === 'left' ? (
+                <Image
+                  src="/cute-whale.svg"
+                  alt="Cute whale"
+                  width={300}
+                  height={200}
+                />
+              ) : null}
+              <div className="w-full flex flex-col gap-y-8 max-w-[50%]">
+                <h3 className="font-bold text-3xl max-w-[900px] leading-tight uppercase">
+                  {item.title}
+                </h3>
+                <p className={`${inter.className} text-base font-extralight`}>
+                  {item.text}
+                </p>
+              </div>
+              {item.imagePosition === 'right' ? (
+                <Image
+                  src="/cute-whale.svg"
+                  alt="Cute whale"
+                  width={300}
+                  height={200}
+                />
+              ) : null}
+            </div>
+          ))}
+        </div>
+        <div className="flex flex-col mt-32 w-full gap-32 text-3xl">
+          <h2 className="w-[50%]">
+            Climate change is disrupting the water cycle in worrying ways,
+            increasing evaporation and resulting in more intense and extreme
+            precipitation events.
+          </h2>
+          <h2 className="w-[50%] self-end">
+            This leads to prolonged droughts in some areas and devastating
+            floods in others.
+          </h2>
+          <h2 className="w-[50%]">
+            Glacier melting affects the supply of freshwater, while rising sea
+            levels cause saline intrusion in coastal aquifers.
+          </h2>
+          <h2 className="w-[50%] self-end">
+            Sustainable water management and reducing greenhouse gas emissions
+            are crucial to preserving this delicate and essential balance for
+            life on Earth.
+          </h2>
+        </div>
+        <hr className="w-full opacity-75" />
+        <div
+          id="practice"
+          className={`${inter.className} w-full flex flex-col gap-16`}
+        >
+          <h1 className="text-5xl w-[75%] text-bold">
+            So far we have learned a lot about the cycle of{' '}
+            <span className="text-[#4A89DC]">water</span>.
+          </h1>
+          <div className="flex justify-between">
+            <Image
+              className="max-w-[50%]"
+              src="/walter.svg"
+              alt="Walter"
+              width={200}
+              height={100}
+            />
+            <h2 className="text-4xl w-[55%] self-end">
+              Scientists also say that practicing is part of the learning
+              process, so we have a{' '}
+              <span className="text-[#5DD2CE]">quick test</span>.
+            </h2>
+          </div>
+          <div className="flex text-2xl gap-12 mt-12">
+            <p>
+              During this <span className="text-[#5DD2CE]">test</span>, you have
+              to choose the correct option for each question in order to keep
+              your <span className="text-[#4A89DC]">water</span> resource.
+            </p>
+            <p>
+              And as you may know, <span className="text-[#4A89DC]">water</span>{' '}
+              is <span className="text-[#5DD2CE]">crucial</span> for human
+              being, so you don&apos;t want to{' '}
+              <span className="text-[#5DD2CE]">lose</span> it, right?
+            </p>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
-  )
+  );
 }
